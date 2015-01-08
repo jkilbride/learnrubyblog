54b36b9a53cac603ac7cce9147833db866fd63c6
John Kilbride <jkilbride@gmail.com>
2015-01-08 16:29:12 +0000

    Commiting Project to Heroku

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+    def create
+        #render plain: params.inspect
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+    
+    def destroy
+        #render plain: params.inspect
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+        
+        
+    end 
+    
+    private 
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
+end
